7c5b5e9429a1784c2b967b3a93d264f32881ab67
ckalia <ckalia@uwaterloo.ca>
2023-02-18 19:02:26 -0500

    added function to calculate missing nutrient amounts and tested entire recommendation process

--- python/mongo_db_queries.py
+++ python/mongo_db_queries.py
@@ -5,6 +5,11 @@ import pandas as pd
 
 from reccommended_intake import get_nutrient_intake
 
+# TODO confirm intake units are same as units of nutrients in intake dataset
+# TODO allergens
+# how to factor in amount of missing nutrients
+
+
 # view stuff
 # can let user select age, name, gender, dietary preferences (checkbox), allergies (checkbox)
 
@@ -39,20 +44,22 @@ def determine_missing_nutrient_amounts(nutrients_in_diet, recommended_intake):
     nutrients_in_diet: summed-up amounts for each nutrient in the user's diet
     recommended_intake: recommended intake for each nutrient
 
-    return: 
+    return: dictionary where key is nutrient and value is amount missing based on RDA
 
     """
-
-    for nutrient, intake in recommended_intake.items():
-        # nutrient should always be found in recommended_intake
-        if nutrient in nutrients_in_diet:
-            rec_intake = intake.get("RDA")
-            up_intake = intake.get("UL")
-            user_intake = nutrients_in_diet.get("nutrient")
-            print(rec_intake, up_intake, user_intake)
-            print("----")
+    missing_nutrients = {}
+    for nutrient, user_intake in nutrients_in_diet.items():
+        if nutrient in recommended_intake:
+            rec_intake = recommended_intake.get(nutrient).get("RDA")
+            up_intake = recommended_intake.get(nutrient).get("UL")
+            if user_intake < rec_intake:
+                missing_nutrients[nutrient] = rec_intake - user_intake
+            # upper limit that doesn't exist, is recored as "ND" in the intake dataset
+            elif type(up_intake) in ("int", "float") and user_intake > up_intake:
+                raise Exception(f"User's {nutrient} intake exceeds upper limit")
         else:
-            raise Exception("nutrient not found")
+            missing_nutrients[nutrient] = 0
+    return missing_nutrients
 
 
 # note: no sorting on score capability with fuzzy match
@@ -101,24 +108,15 @@ def find_food(search_query, limit):
 
     return res
 
-a = find_food("frot", 1)
-b = sum_nutrient_values(a)
-c = get_nutrient_intake(19)
-d = determine_missing_nutrient_amounts(b, c)
-print(d)
 
-
-# print(a)
-
-
-nutrients = ["Protein", "Carbohydrate"]
+# nutrients = ["Protein", "Carbohydrate"]
 # TODO how to take into account allergens
-def get_food_from_nutrients(nutrients, dietary_preferences, allergens):
+def get_food_from_nutrients(nutrients, dietary_preferences, allergens = ""):
     """
     returns a list of foods and their nutrients such that atleast one nutrient matches
 
     nutrients: list of nutrients that aren't being met
-    dietary_preferences: list of dietary_preferences for a user
+    dietary_preferences: dictionary of dietary_preferences for a user
     allergies: list of allergens for a user
 
     return: list of dictionaries. each dictionary contains food_name, food_weight, and nutrients list.
@@ -162,3 +160,15 @@ def get_food_from_nutrients(nutrients, dietary_preferences, allergens):
 
 # a = get_food_from_nutrients(nutrients, is_vegan, is_vegetarian)
 # print(a)
+
+
+def main():
+    a = find_food("Fuji Apple", 2)
+    b = sum_nutrient_values(a)
+    c = get_nutrient_intake(19)
+    d = determine_missing_nutrient_amounts(b, c)
+    dietary_preferences = {"is_vegan": True, "is_vegetarian": True}
+    missing_nutrients_list = list(d.keys())
+    e = get_food_from_nutrients(missing_nutrients_list, dietary_preferences)
+    return e[0]
+print(main())
