16007f72901146bb9ccf7625177b29bebac13bd3
Matt Ruegg <matt.ruegg9@gmail.com>
2023-03-02 00:18:53 -0500

    sliders and text box inserts finally working

--- flet/dropdowns_again.py
+++ flet/dropdowns_again.py
@@ -12,14 +12,25 @@ async def main(page: ft.Page):
          ft.dropdown.Option("5"),
     ]
 
+    t = ft.Text()
+
     chosen_foods = {}
     available_foods = ['apple', 'pear', 'orange', 'kiwi', 'cherry']
+
+    async def textbox_results(e):
+        t.value = f"Typing in {e.control.label}; typed {e.control.value}"
+        await page.add_async(t)
+    
+    async def slider_results(e):
+        t.value = f"Changed {e.control.data}; slid to {e.control.value}"
+        await page.add_async(t)
 
     async def start_program(e):
         await page.clean_async()
         await page.add_async(food_dropdown)
         for x in chosen_foods:
-            await page.add_async(ft.Dropdown(label=x, options=nums))
+            await page.add_async(ft.TextField(label=x, on_change=textbox_results))
+            await page.add_async(ft.Slider(min = 1, max = 5, divisions = 4, value = 2, label = "{value}", data = x, on_change_end=slider_results))
         await page.update_async()
 
     async def find_option(option_name):
