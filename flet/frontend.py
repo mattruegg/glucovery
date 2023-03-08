4afffba09a65783837cdf0a9c5ec8914a63b287a
Matt Ruegg <matt.ruegg9@gmail.com>
2023-03-07 22:45:12 -0500

    selected foods and symptoms have 'X' button to remove and readd them to dropdown foods

--- flet/frontend.py
+++ flet/frontend.py
@@ -9,13 +9,22 @@ async def main(page: ft.Page):
     chosen_symptoms = {}
     available_symptoms = {'headache', 'numbness', 'nausea'}
 
-    # FIRST PAGE
+    #
+    # --------------------------------------- FOODS PAGE ---------------------------------------
+    #
+
     async def foods_page(e):
         await page.clean_async()
         await page.add_async(ft.Row([mongo_food_search, search_mongo_foods_button]))
         await page.add_async(food_dropdown)
         for x in chosen_foods:
-            await page.add_async(ft.TextField(label=x, on_change=textbox_results, value=chosen_foods[x]))
+            await page.add_async(ft.Row(
+                [
+                    ft.TextField(label=x, on_change=textbox_results,
+                                 value=chosen_foods[x]),
+                    ft.ElevatedButton("X", on_click=remove_chosen_food, data=x)
+                ]
+            ))
         await page.add_async(go_to_symptoms)
         await page.update_async()
 
@@ -29,12 +38,12 @@ async def main(page: ft.Page):
     async def choose_food_from_dropdown(e):
         chosen_foods[food_dropdown.value] = ""
         await page.update_async()
-        option = await find_option(food_dropdown.value)
+        option = await find_food_option(food_dropdown.value)
         if option != None:
             food_dropdown.options.remove(option)
         await foods_page(e)
 
-    async def find_option(option_name):
+    async def find_food_option(option_name):
         for option in food_dropdown.options:
             if option_name == option.key:
                 return option
@@ -45,7 +54,16 @@ async def main(page: ft.Page):
         chosen_foods[e.control.label] = e.control.value
         print(chosen_foods)
 
-    # SECOND PAGE
+    # REMOVE CHOSEN FOOD
+    async def remove_chosen_food(e):
+        chosen_foods.pop(e.control.data)
+        food_dropdown.options.append(ft.dropdown.Option(e.control.data))
+        await foods_page(e)
+
+    #
+    # --------------------------------------- SYMPTOMS PAGE ---------------------------------------
+    #
+
     async def symptoms_page(e):
         await page.clean_async()
         await page.add_async(ft.Row([mongo_symptom_search, search_mongo_symptoms_button]))
@@ -54,7 +72,9 @@ async def main(page: ft.Page):
             await page.add_async(ft.Row(
                 [
                     ft.Text(x),
-                    ft.Slider(min=1, max=5, divisions=4, value=chosen_symptoms[x], label="{value}", data=x, on_change_end=slider_results),
+                    ft.Slider(min=1, max=5, divisions=4,
+                              value=chosen_symptoms[x], label="{value}", data=x, on_change_end=slider_results),
+                    ft.ElevatedButton("X", on_click=remove_chosen_symptom, data=x)
                 ]
             ))
         await page.update_async()
@@ -69,40 +89,50 @@ async def main(page: ft.Page):
     async def choose_symptom_from_dropdown(e):
         chosen_symptoms[symptom_dropdown.value] = ""
         await page.update_async()
-        option = await find_option(symptom_dropdown.value)
+        option = await find_symptom_option(symptom_dropdown.value)
         if option != None:
             symptom_dropdown.options.remove(option)
         await symptoms_page(e)
 
-    async def find_option(option_name):
+    async def find_symptom_option(option_name):
         for option in symptom_dropdown.options:
             if option_name == option.key:
                 return option
         return None
-    
+
     # STORE SYMPTOM VALUES
     async def slider_results(e):
         chosen_symptoms[e.control.data] = e.control.value
         print(chosen_symptoms)
 
-    # INITIALIZE VARIABLES
+    # REMOVE CHOSEN FOOD
+    async def remove_chosen_symptom(e):
+        chosen_symptoms.pop(e.control.data)
+        symptom_dropdown.options.append(ft.dropdown.Option(e.control.data))
+        await symptoms_page(e)
+    
+    #
+    # --------------------------------------- INITIALIZE VARIABLES ---------------------------------------
+    #
+
     mongo_food_search = ft.TextField()
     search_mongo_foods_button = ft.ElevatedButton(
         "Search", on_click=search_for_foods)
-    
-    mongo_symptom_search = ft.TextField()
-    search_mongo_symptoms_button = ft.ElevatedButton(
-        "Search", on_click=search_for_symptoms)
 
     food_dropdown = ft.Dropdown(on_change=choose_food_from_dropdown)
     for x in available_foods:
         food_dropdown.options.append(ft.dropdown.Option(x))
-        
+
+    mongo_symptom_search = ft.TextField()
+    search_mongo_symptoms_button = ft.ElevatedButton(
+        "Search", on_click=search_for_symptoms)
+
     symptom_dropdown = ft.Dropdown(on_change=choose_symptom_from_dropdown)
     for x in available_symptoms:
         symptom_dropdown.options.append(ft.dropdown.Option(x))
 
-    go_to_symptoms = ft.ElevatedButton("Go To Symptoms", on_click=symptoms_page)
+    go_to_symptoms = ft.ElevatedButton(
+        "Go To Symptoms", on_click=symptoms_page)
 
     # SET UP START OF PROGRAM
     await foods_page(e=any)
